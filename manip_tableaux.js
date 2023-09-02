8147984c114484d813a5070fa33fc1ca4f870dcd
Amath2605 <amadoudiatta734@gmail.com>
2023-09-02 15:06:55 +0000

    ch
    changed1

---
+++ manip_tableaux.js
@@ -0,0 +1,8 @@
+let playlist = ["A kind of magic", "We are the champions", "The show must go on"]
+
+playlist.push("Innuendo")
+playlist.push("Radio Gaga")
+
+playlist.pop()
+
+console.log(playlist)
